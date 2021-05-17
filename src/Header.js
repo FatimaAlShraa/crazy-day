8bc8f48fd06a607def6cb07f2408f77f48c83de3
FatimaAlShraa <falhmood66@gmail.com>
2021-05-17 15:01:59 +0300

    change result

--- src/Header.js
+++ src/Header.js
@@ -3,9 +3,9 @@ import React from 'react';
 class Header extends React.Component{
     render(){
         return(
-            <header>
+           
             <h1>Gallery Of horn</h1>
-            </header>
+           
         )
     }
 }
